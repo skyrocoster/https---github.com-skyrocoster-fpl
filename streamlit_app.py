7e12c771e346602b50b9bf8e3ef94f51816d95cb
skyrocoster <91031422+skyrocoster@users.noreply.github.com>
2023-02-04 16:27:12 +0000

    Streamlit building

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,37 @@ sidebar = st.sidebar
 app_context = app.app_context()
 app_context.push()
 
+player_avg_fields = [
+    "was_home",
+    "minutes",
+    "goals_scored",
+    "assists",
+    "clean_sheets",
+    "goals_conceded",
+    "own_goals",
+    "penalties_saved",
+    "penalties_missed",
+    "yellow_cards",
+    "red_cards",
+    "saves",
+    "bonus",
+    "bps",
+    "influence",
+    "creativity",
+    "threat",
+    "ict_index",
+    "expected_goals",
+    "expected_assists",
+    "expected_goal_involvements",
+    "expected_goals_conceded",
+    "selected",
+    "goals_assists",
+]
+
+# player_fixture_fields = [
+
+# ]
+
 
 @st.cache
 def load_team_list():
@@ -38,6 +69,18 @@ def load_player_fixtures():
     elif sel_home == "Away":
         df = df.loc[df["was_home"] == False]
     return df
+
+
+@st.cache
+def load_player_avgs(df, keep_cols=player_avg_fields):
+    df_avgs = df[keep_cols]
+    df_avgs = (
+        df_avgs.loc[df_avgs["minutes"] > 60]
+        .groupby(["web_name"])
+        .mean(numeric_only=True)
+    )
+    df_avgs = df_avgs.reset_index()
+    return df_avgs
 
 
 @st.cache
@@ -73,9 +116,50 @@ with sidebar:
         horizontal=True,
     )
 
-df = load_player_fixtures()
-df_avg = df.loc[df["minutes"] > 60].groupby(["web_name"]).mean(numeric_only=True)
-st.subheader("By Fixture")
-st.dataframe(df)
-st.subheader("Player Averages")
-st.dataframe(df_avg)
+player_fixtures = load_player_fixtures()
+tab1, tab2 = st.tabs(["Dataframe", "Scatter Plot"])
+with tab1:
+    st.subheader("By Fixture")
+    st.dataframe(player_fixtures)
+with tab2:
+    player_fixtures = player_fixtures.sort_values(["gameweek_id", "player_id"])
+    fig = px.line(
+        player_fixtures,
+        x="gameweek_id",
+        y="gameweek_points",
+        color=player_fixtures.index,
+        markers=True
+        # size=sel_player_avg_size,
+    )
+    fig.update_layout(xaxis=dict(tickmode="linear", tick0=1, dtick=1))
+    fig.update_traces(connectgaps=False)
+    st.plotly_chart(fig)
+
+player_fixtures_avg = load_player_avgs(player_fixtures)
+tab1, tab2 = st.tabs(["Dataframe", "Scatter Plot"])
+with tab1:
+    st.subheader("Player Averages")
+    st.dataframe(player_fixtures_avg)
+with tab2:
+    col1, col2 = st.columns(2)
+    with col1:
+        sel_player_avg_x = st.selectbox(
+            "Select X Axis", player_avg_fields, key="sel_player_avg_x"
+        )
+    with col2:
+        sel_player_avg_y = st.selectbox(
+            "Select Y Axis", player_avg_fields, key="sel_player_avg_y"
+        )
+        sel_player_avg_size = st.selectbox(
+            "Select Size of Trace", player_avg_fields, key="sel_player_avg_size"
+        )
+
+    fig = px.scatter(
+        player_fixtures_avg,
+        x=sel_player_avg_x,
+        y=sel_player_avg_y,
+        color="web_name",
+        size=sel_player_avg_size,
+        trendline="ols",
+    )
+    st.plotly_chart(fig)
